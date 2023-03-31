bda6a16cec0e4c914d53c6e27d6033740e640c00
Josh Holmes <josh@glowingmanagement.com>
2023-03-31 19:00:15 +0100

    remove js files

---
+++ webapp/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+interface Props {
+  // Define the props that will be passed to the component here
+}
+
+const HomePage: FC<Props> = (props) => {
+  // Define the component logic here
+  return (
+    <div>
+      Home Page
+    </div>
+  );
+}
+
+export default HomePage;
