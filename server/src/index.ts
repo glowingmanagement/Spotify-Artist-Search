41a737e039a28dfe808cccb926d3a9171971bbb8
Josh Holmes <josh@glowingmanagement.com>
2023-03-31 13:54:06 +0100

    set up express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
