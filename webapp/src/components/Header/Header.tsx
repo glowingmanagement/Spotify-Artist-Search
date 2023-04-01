6cd086a515d0a56ec4bac53e41c00b929c2ceff1
Josh Holmes <josh@glowingmanagement.com>
2023-04-01 19:01:29 +0100

    update UX

--- webapp/src/components/Header/Header.tsx
+++ webapp/src/components/Header/Header.tsx
@@ -1,18 +1,25 @@
-import UnHurdLogo from '../../assets/images/logo.svg';
-import './Header.css';
-
+import UnHurdLogo from "../../assets/images/logo.svg";
+import "./Header.css";
 
 const Header = () => {
-  // Define the component logic here
+  const navigateHome = () => {
+    window.location.href = "/";
+  };
+
   return (
-    <div className='headerContainer'>
-        <div className='navLeft'>
-            <img src={UnHurdLogo} alt="UnHurd Logo" className='logo'/>
-            <div className='divider'></div>
-            <h1 className='headerLeftText'>The #1 music marketing app</h1>
-        </div>
+    <div className="headerContainer">
+      <div className="navLeft">
+        <img
+          src={UnHurdLogo}
+          alt="UnHurd Logo"
+          className="logo"
+          onClick={() => navigateHome()}
+        />
+        <div className="divider"></div>
+        <h1 className="headerLeftText">The #1 music marketing app</h1>
+      </div>
     </div>
   );
-}
+};
 
 export default Header;
